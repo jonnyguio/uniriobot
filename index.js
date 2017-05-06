96fd00c967cab99e1e2d029c7e68d9288256cc14
João Luis da Silva Guio Soares <joaoluisguio@gmail.com>
2017-05-06 15:21:46 -0300

    Token -> pageToken.

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function sendTextMessage(sender, text) {
     let messageData = { text:text }
     request({
 	    url: 'https://graph.facebook.com/v2.6/me/messages',
-	    qs: {access_token:token},
+	    qs: {access_token:pageToken},
 	    method: 'POST',
 		json: {
 		    recipient: {id:sender},
