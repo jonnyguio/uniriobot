edb35303ffbd535c6badec92715895b9a49a92b8
yagoazedias <yagoazedias@gmail.com>
2017-05-06 18:13:11 -0300

    Added postgres.js and pg client to package.json

---
+++ config/postgres.js
@@ -0,0 +1,19 @@
+var pg = require('pg');
+var conString = "postgres://yagoazedias:admin123@@localhost/cardapio";
+
+pg.connect(conString, function(err, client, done) {
+
+  if (err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT * from segunda_feira', function(err, result) {
+    done();
+    if (err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].guarnicao);
+  });
+
+});
+
+
