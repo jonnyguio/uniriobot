72eeaf9d2f453d328947a8413655dcd86c4f9ab5
João Luis da Silva Guio Soares <joaoluisguio@gmail.com>
2017-05-07 11:09:15 -0300

    Remove accents...

--- sender.js
+++ sender.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const pg = require('pg');
 const removePunctuation = require('remove-punctuation');
+const removeAccents = require('diacritics').remove;
 const pageToken = process.env.PAGETOKEN;
 
 const API_UNIRIO_URL = 'http://sistemas.unirio.br/api_teste/'
