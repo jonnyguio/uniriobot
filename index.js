fcf0352c39f459852d27eacbe9ccd4bee16269b5
yagoazedias <yagoazedias@gmail.com>
2017-05-07 13:43:15 -0300

    Update index.html and create public folder

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const csvToJson = require('csvtojson')
-const https = require('https')
+const csvToJson = require('csvtojson');
+const https = require('https');
 
 const receiveHandler = require('./receiver');
-const sendHandler = require('./sender')
+const sendHandler = require('./sender');
 const app = express();
 
 var consign = require('consign');
